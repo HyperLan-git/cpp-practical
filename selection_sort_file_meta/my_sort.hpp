0b8760f57dedca35ffaa13e89751eb927831fa91
HyperLan-git <youness.rekik@epita.fr>
2022-11-24 17:55:21 +0100

    fixed code

--- selection_sort_file_meta/my_sort.hpp
+++ selection_sort_file_meta/my_sort.hpp
@@ -1,37 +1,30 @@
-#include <iostream>
 template <typename iterator>
 void my_selection_sort(iterator begin, iterator end) {
     typename std::remove_pointer<typename iterator::value_type>::type a;
 
     if constexpr (std::is_integral<typeof(a)>::value) {
+        size_t evens = 0;
+        iterator odds = begin;
         for (iterator i = begin; i != end; i++) {
-            iterator minEven = i;
-            for (iterator it = i; it != end; it++) {
-                auto val = *it;
-                if (((val % 2) == 0) && val < *minEven) minEven = it;
+            if (*i % 2 == 0) {
+                std::swap(*i, *odds++);
+                evens++;
             }
+        }
+        for (iterator j = begin; j != odds; j++) {
+            iterator min = j;
+            for (iterator it = ++iterator(j); it != odds; it++)
+                if (*it < *min) min = it;
 
-            if (((*minEven) % 2) == 0) {
-                typeof(a) tmp = *minEven;
-                *minEven = *i;
-                *i = tmp;
-            }
-            for (iterator j = begin; j != end; j++) std::cout << *j << ' ';
-            std::cout << std::endl;
-        } /*
-         iterator rend = begin;
-         rend--;
-         for (iterator i = --iterator(end); i != rend; i--) {
-             iterator maxOdd = i;
-             for (iterator it = i; it != end; it--) {
-                 auto val = *it;
-                 if (val % 2 == 0 && val < *maxOdd) maxOdd = it;
-             }
+            std::swap(*j, *min);
+        }
+        for (iterator j = odds; j != end; j++) {
+            iterator min = j;
+            for (iterator it = ++iterator(j); it != end; it++)
+                if (*it < *min) min = it;
 
-             auto tmp = *maxOdd;
-             *maxOdd = *i;
-             *i = tmp;
-         }*/
+            std::swap(*j, *min);
+        }
     } else
         while (begin != end) {
             iterator min = begin;
